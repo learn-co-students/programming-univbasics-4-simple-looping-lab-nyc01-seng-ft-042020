53adb50602cbd13c7f01d04ba120ffa68be42aa8
James Jiang <monochrome473@gmail.com>
2020-04-11 18:43:30 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,30 @@
 # Write your methods here
+require 'pry'
+def loop_message_five_times(string)
+  5.times do
+    puts string
+  end
+end
+
+def loop_message_n_times(string, number)
+  number.times do
+    puts string
+  end
+end
+
+def output_array(array)
+  counter = 0
+  while array[counter] do
+    puts array[counter]
+    counter += 1
+  end
+end
+
+def return_string_array(array)
+  counter = 0
+  while array[counter] do
+    array[counter] = "#{array[counter]}"
+    counter += 1
+  end
+  return array
+end
