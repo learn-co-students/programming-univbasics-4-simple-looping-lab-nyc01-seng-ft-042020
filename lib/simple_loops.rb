72834865360d8cb99c0f2bdfc180104a4bd1f981
Beau Troxclair <beau.troxclair@gmail.com>
2020-04-15 11:04:34 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,49 @@
 # Write your methods here
+def loop_message_five_times(message)
+  n=0
+  while n < 5 do
+    # possible security flaw. 
+    # message is allowed to be of arbitrary size. 
+    # Write check for valid input
+    puts message
+    n += 1
+  end
+end
+
+
+def loop_message_n_times(message, i)
+  n=0
+  while n < i do
+    # possible security flaw. 
+    # message is allowed to be of arbitrary size. 
+    # Write check for valid input
+    puts message
+    n += 1
+  end
+end
+
+def output_array(array)
+  n=0
+  arr = []
+  while n < array.length do
+    # possible security flaw. 
+    # message is allowed to be of arbitrary size. 
+    # Write check for valid input
+    puts array[n]
+    n += 1
+  end
+  arr
+end
+
+def return_string_array(array)
+  n=0
+  arr = []
+  while n < array.length do
+    # possible security flaw. 
+    # message is allowed to be of arbitrary size. 
+    # Write check for valid input
+    arr.push("#{array[n]}")
+    n += 1
+  end
+  arr
+end
